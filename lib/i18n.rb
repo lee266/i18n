615c912f2fd01fe94f577b6cc444b407645785b8
rito.ishihara <rito.ishihara@optim.co.jp>
2026-02-15 16:45:42 +0900

    feat(config): introduce immutable config flow for fiber safety

--- lib/i18n.rb
+++ lib/i18n.rb
@@ -73,16 +73,30 @@ module I18n
       end
     end
 
+    # Gets a mutable I18n configuration object.
+    def writable_config
+      current = config
+      return current unless current.frozen?
+
+      current = current.dup
+      if Fiber.respond_to?(:[])
+        Fiber[:i18n_config] = current
+      else
+        Thread.current.thread_variable_set(:i18n_config, current)
+      end
+      current
+    end
+    private :writable_config
+
     # Sets I18n configuration object.
     def config=(value)
       if Fiber.respond_to?(:[])
         Fiber[:i18n_config] = value
-        value.owner = Fiber.current if value.respond_to?(:owner=)
+        value.owner = Fiber.current if value.respond_to?(:owner=) && !value.frozen?
       else
         Thread.current.thread_variable_set(:i18n_config, value)
       end
     end
-
     # Write methods which delegates to the configuration object
     %w(locale backend default_locale available_locales default_separator
       exception_handler load_path enforce_available_locales).each do |method|
@@ -92,7 +106,7 @@ module I18n
         end
 
         def #{method}=(value)
-          config.#{method} = (value)
+          writable_config.#{method} = (value)
         end
       DELEGATORS
     end
